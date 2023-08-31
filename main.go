379cb42af2f56c557a0aeadd839d8a6295fab817
Andrew Snodgrass <git@pointlander.info>
2023-08-30 22:44:51 -0500

    WIP detector

--- main.go
+++ main.go
@@ -5,12 +5,35 @@
 package main
 
 import (
+	"encoding/gob"
+	"flag"
 	"fmt"
+	"math"
+	"os"
+	"time"
 
 	"github.com/samuel/go-hackrf/hackrf"
 )
 
+var (
+	// FlagLearn a point
+	FlagLearn = flag.String("learn", "", "learn a point")
+	// FlagInfer
+	FlagInfer = flag.Bool("infer", false, "inference mode")
+)
+
+// Point is a point
+type Point struct {
+	Name   string
+	Points [][]byte
+}
+
+// Points is a set of points
+type Points map[string]Point
+
 func main() {
+	flag.Parse()
+
 	err := hackrf.Init()
 	if err != nil {
 		panic(err)
@@ -21,5 +44,78 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Device opened")
+	device.SetFreq(1e6)
 	defer device.Close()
+
+	input, err := os.Open("points.gob")
+	points := make(Points)
+	if err == nil {
+		decoder := gob.NewDecoder(input)
+		err = decoder.Decode(&points)
+		if err != nil {
+			panic(err)
+		}
+	}
+	input.Close()
+
+	if *FlagLearn != "" {
+		fmt.Println("wait 5 seconds")
+		time.Sleep(5 * time.Second)
+
+		err = device.StartRX(func(buff []byte) error {
+			entry := points[*FlagLearn]
+			entry.Name = *FlagLearn
+			entry.Points = append(entry.Points, buff)
+			fmt.Println(len(buff))
+			points[*FlagLearn] = entry
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+
+		time.Sleep(10 * time.Second)
+		err = device.StopRX()
+		if err != nil {
+			panic(err)
+		}
+
+		output, err := os.Create("points.gob")
+		if err != nil {
+			panic(err)
+		}
+		defer output.Close()
+		encoder := gob.NewEncoder(output)
+		err = encoder.Encode(points)
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	if *FlagInfer {
+		err = device.StartRX(func(buff []byte) error {
+			name, min := "", math.MaxFloat64
+			for _, entry := range points {
+				for _, point := range entry.Points {
+					sum := 0.0
+					for key, value := range buff {
+						diff := float64(point[key]) - float64(value)
+						sum += diff * diff
+					}
+					if sum < min {
+						min, name = sum, entry.Name
+					}
+				}
+			}
+			fmt.Printf("%s %f\n", name, min)
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+		for {
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
